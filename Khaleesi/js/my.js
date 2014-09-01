7dce7b90c797f66775ce5edffb781eef9dbb4d88
felot_g <felot_g@epitech.eu>
2014-09-01 16:02:31 +0200

    Modif Norm

--- Khaleesi/js/my.js
+++ Khaleesi/js/my.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#fullPage').fullpage( {
-        sectionsColor: ['#FFF', '#1BBC9B', '#7E8F7C'],
+        sectionsColor: ['#FFF', '#FFF', '#FFF'],
         anchors: ['accueil', 'dataflux', 'turfu'],
         // easing: 'easeInOutBounce',
         css3: true,
@@ -11,11 +11,27 @@ $(document).ready(function() {
             //playing the video
             $('video').get(0).play();
         },
-        scrollOverflow: true
+        scrollOverflow: true,
+        afterLoad: function(anchorLink, index){
+            if(index == '1'){
+                $('#menu1').css('color:#000');
+                $('#menu2').css('color:#ababab');
+                $('#menu3').css('color:#ababab');
+            }
+            if(index == '2'){
+                $('#menu1').css('color:#ababab');
+                $('#menu2').css('color:#000');
+                $('#menu3').css('color:#ababab');
+            }
+            if(index == '3'){
+                $('#menu1').css('color:#ababab');
+                $('#menu2').css('color:#ababab');
+                $('#menu3').css('color:#000');
+            }
+        }
     });
 
     $('#arrowDown1').addClass('animated fadeInDown');
     $('#arrowDown2').addClass('animated fadeInDown');
     $('#arrowDown3').addClass('animated fadeInDown');
-    
 });
